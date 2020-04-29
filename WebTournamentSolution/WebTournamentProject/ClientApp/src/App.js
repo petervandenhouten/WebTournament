9e1f2d5604af59df1b5d998ad1a1b52821aa4f26
pvdhouten <pvdhouten@yahoo.com>
2020-04-29 14:04:59 -0300

    Clean up code from template.

--- WebTournamentSolution/WebTournamentProject/ClientApp/src/App.js
+++ WebTournamentSolution/WebTournamentProject/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Teams } from './components/Teams';
 import { Results } from './components/Results';
 import { Table } from './components/Table';
@@ -18,8 +16,6 @@ export default class App extends Component {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/teams' component={Teams} />
         <Route path='/results' component={Results} />
         <Route path='/table' component={Table} />
